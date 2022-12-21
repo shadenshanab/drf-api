61dc3cdb227ceace8b6ceb7323c4a9285df4f2d6
Shaden Shanab <shadenshanab2@gmail.com>
2022-12-21 17:46:26 +0000

    done

--- country/urls.py
+++ country/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import CountryListView, CountryDetailView
+
 urlpatterns = [
     path('', CountryListView.as_view(),),
     path('<int:pk>', CountryDetailView.as_view(),)
